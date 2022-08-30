96cc7caba4aab87e6b0f347267aae81caccdcf5a
Stephan Kern <stephan@swmmrman.com>
2022-08-30 14:16:58 -0600

    First runnable client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use openrgb::OpenRGB;
+use std::error::Error;
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = OpenRGB::connect().await?;
+    Ok(())
 }
