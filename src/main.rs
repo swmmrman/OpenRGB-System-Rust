85978d39d78bd7e76373815b4657f69829dba8e2
Stephan Kern <stephan@swmmrman.com>
2022-09-06 18:52:55 -0600

    change and restore

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use openrgb::{
     data::Color,
     OpenRGB,
 };
+use std::{thread, time};
 
 use std::error::Error;
 use tokio;
@@ -10,5 +11,17 @@ use tokio;
 async fn main() -> Result<(), Box<dyn Error>> {
     let client = OpenRGB::connect().await?;
     client.set_name("OpenRGB System Rust").await?;
+    let keyboard = client.get_controller(0).await?;
+    //Lets store the current config to restore later.
+    let orig_colors = keyboard.colors;
+    let _orig_mode = keyboard.active_mode;
+    let mut new_colors = vec![];
+    for _ in 0..117 {
+        new_colors.push(Color::new(255,0,0));
+    }
+    client.update_leds(0, new_colors.to_vec()).await?;
+    thread::sleep(time::Duration::from_secs(1));
+    //client.update_mode(0,2);
+    client.update_leds(0, orig_colors).await?;
     Ok(())
 }
