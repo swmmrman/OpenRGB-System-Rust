3f781704facaf753d8e28c6bfebce8b92a482b47
Stephan Kern <stephan@swmmrman.com>
2022-09-06 18:00:36 -0600

    Drawing random colors on the keyboard to test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,47 @@
-use openrgb::OpenRGB;
+use openrgb::{
+    data::Color,
+    OpenRGB,
+};
+
 use std::error::Error;
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let client = OpenRGB::connect().await?;
+    client.set_name("OpenRGB System Rust").await?;
+    let _count = client.get_controller_count().await?;
+    //println!("{}", count);
+    
+    let controller = client.get_controller(0).await?;
+    let mut colors = controller.colors;
+    for i in 0..117 {
+        colors[i] = Color::new(0,0,0);
+    }
+    for i in 0..26 {
+        colors[i] = Color::new(255,0,0);
+    }
+    for i in 26..36 {
+        colors[i] = Color::new(0,0,255);
+    }
+    for i in 36..75 {
+        colors[i] = Color::new(0,255,0);
+    }
+    for i in 75..79 {
+        colors[i] = Color::new(255,200,0);
+    }
+    for i in 79..95 {
+        colors[i] = Color::new(0,255,0);
+    }
+    for i in 95..112 {
+        colors[i] = Color::new(0,255,255);
+    }
+    for i in 112..117 {
+        colors[i] = Color::new(255,0,255);
+    }
+    let _led_count = colors.len();
+    client.update_leds(0, colors).await?;
+    //println!("{}", led_count);
+    //println!("{:#?}", controller.leds);
     Ok(())
 }
