4306cd4b363423044eff3a9725644e969b2a05d1
Stephan Kern <stephan@swmmrman.com>
2022-10-02 16:06:29 -0600

    Add cpu temp averaging to smooth out flicker

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::io::{self, Write};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use tokio;
-
+use std::collections::VecDeque;
 
 fn get_cpu_file() -> Result<String, io::Error> {
     let mut cpufile = String::new();
@@ -114,6 +114,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     client.update_leds(0, colors.to_vec()).await?;
     
     let mut sys = System::new_all();
+    let mut cpu_vals: VecDeque<f32> = VecDeque::from([0.32; 10]);
     while running.load(Ordering::SeqCst) {
         print!("\r");
         //fans start at keys[21]
@@ -130,7 +131,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
             colors[indexs[i]] = get_color(core.cpu_usage() / 100.0);
             i = i + 1; 
         }
-        colors[indexs[20]] = get_color(((get_cpu_temp(&cpu_file) - 24.0)*1.4) / 100.0);
+        cpu_vals.pop_front();
+        cpu_vals.push_back(((get_cpu_temp(&cpu_file) - 24.0)*1.4) / 100.0);
+        let cpu_avg = cpu_vals.iter().sum::<f32>() / 10.0;
+        colors[indexs[20]] = get_color(cpu_avg);
         io::stdout().flush().unwrap();
         client.update_leds(0, colors.to_vec()).await?;
         thread::sleep(time::Duration::from_millis(100));
