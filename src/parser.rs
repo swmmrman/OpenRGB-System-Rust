f0d2be9f54ed3a29f9f2753646c7267167cbf781
Stephan Kern <stephan@swmmrman.com>
2022-10-03 03:13:43 -0600

    Refactor so setting reseting system colors is out of main

--- src/parser.rs
+++ src/parser.rs
@@ -8,14 +8,21 @@ use std::{fs,thread};
 use std::time::Duration;
 use tokio;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn change_sys_color(color: Color) -> Result<(), Box<dyn Error>> {
     let client = OpenRGB::connect().await?;
     let lights = client.get_controller(1).await?;
     let mut colors = Vec::new();
     for _ in 0..lights.colors.len() {
-        colors.push(Color::new(255,255,255));
+        colors.push(color);
     }
     client.update_leds(1, colors).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    change_sys_color(Color::new(255,255,255)).await?;
+    thread::sleep(Duration::from_secs(5));
+    change_sys_color(Color::new(8,2,0)).await?;
     Ok(())
 }
