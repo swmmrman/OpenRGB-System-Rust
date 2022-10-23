75c6ebc4d5e2926fe492249aaacec862e964b963
Stephan Kern <stephan@swmmrman.com>
2022-10-23 01:38:14 -0600

    updates due to graphics card now working but buggy

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use openrgb_system_rust;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let target_controller = 1;
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
     ctrlc::set_handler(move || {
@@ -24,7 +25,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     thread::sleep(time::Duration::from_secs(1));
     let client = OpenRGB::connect().await?;
     client.set_name("OpenRGB System Rust").await?;
-    let keyboard = client.get_controller(0).await?;
+    let keyboard = client.get_controller(target_controller).await?;
     let mut colors = keyboard.colors.to_vec();
     let cpu_file = openrgb_system_rust::get_cpu_file().unwrap();
     let keys = vec!(
@@ -64,17 +65,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let cpu_avg = cpu_vals.iter().sum::<f32>() / 10.0;
         colors[indexs[20]] = openrgb_system_rust::get_color(cpu_avg);
         io::stdout().flush().unwrap();
-        client.update_leds(0, colors.to_vec()).await?;
+        client.update_leds(target_controller, colors.to_vec()).await?;
         thread::sleep(time::Duration::from_millis(100));
     }
     thread::sleep(time::Duration::from_millis(100));
     let exit_color = Color::new(63, 0, 0);
-    for c in 0..client.get_controller_count().await? {
-        let mut exit_colors = Vec::new();
-        for _ in 0..client.get_controller(c).await?.colors.len() {
-            exit_colors.push(exit_color);
-        }
-        client.update_leds(c, exit_colors).await?;
+    for c in 1..client.get_controller_count().await? {
+         let mut exit_colors = Vec::new();
+         for _ in 0..client.get_controller(c).await?.colors.len() {
+             exit_colors.push(exit_color);
+         }
+         client.update_leds(c, exit_colors).await?;
     }
     Ok(())
 }
